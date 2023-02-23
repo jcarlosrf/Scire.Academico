36955d38f9270cc40415e9f58aa1661f01f671a3
Jorge C R Freitas <jcarlosrf@gmail.com>
2023-02-22 22:17:07 -0300

    [Scire] ]criacao do projeto academico

---
+++ ScireAcademico/ScireAcademico/ScireAcademico/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ScireAcademico.ViewModels;
+using ScireAcademico.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ScireAcademico
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ ScireAcademico/ScireAcademico/ScireAcademico/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScireAcademico.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
